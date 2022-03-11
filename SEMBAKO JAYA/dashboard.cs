c346d261dfe0cbc7c09fb9814114d874a7079b0c
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2022-03-11 22:11:53 +0700

    Update dashboard.cs

--- SEMBAKO JAYA/dashboard.cs
+++ SEMBAKO JAYA/dashboard.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 namespace SEMBAKO_JAYA
 {
 	/// <summary>
-	/// Description of dashboard.
+	/// Halaman utama kasir ketuka sesudah melakukan Login
 	/// </summary>
 	public partial class dashboard : Form
 	{
@@ -23,27 +23,40 @@ namespace SEMBAKO_JAYA
 		}
 		void button1_Click(object sender, EventArgs e)
 		{
+		///codingan untuk menyembunyikan halaman dashboard.cs
 			this.Hide();
+			///Class dabarang berguna untuk pemanggilan form Databarang.cs
 			databarang dabarang = new databarang();
+			//menampilkan halaman databarang.cs 
 			dabarang.Show();
 	
 		}
+		///button untuk sign out
 		void button4_Click(object sender, EventArgs e)
 		{
+		///codingan untuk menyembunyikan halaman dashboard.cs
 			this.Hide();
+			///Class signout berguna untuk pemanggilan form LoginForm.cs 
 			LoginForm signout = new LoginForm();
+			//menampilkan halaman Login.cs 
 			signout.Show();
 		}
 		void button3_Click(object sender, EventArgs e)
 		{
+		///codingan untuk menyembunyikan halaman dashboard.cs
 			this.Hide();
+			///Class manajadmin berguna untuk pemanggilan form ManajemenAdmin.cs 
 			ManajemenAdmin manajadmin = new ManajemenAdmin();
+			//menampilkan halaman ManajemenAdmincs 
 			manajadmin.Show();
 		}
 		void button2_Click(object sender, EventArgs e)
 		{
+		///codingan untuk menyembunyikan halaman dashboard.cs
 			this.Hide();
+			///Class inputpemb berguna untuk pemanggilan form Manajemen_Pembeli.cs 
 			Manajemen_Pembeli inputpemb = new Manajemen_Pembeli();
+			//menampilkan halaman Manajemen_Pembeli.cs 
 			inputpemb.Show();
 		}
 	}
