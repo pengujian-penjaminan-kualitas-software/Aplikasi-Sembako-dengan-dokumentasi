dae01d36e048c6c74104f8cdbaf9456c0b5edd59
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2022-03-12 16:13:49 +0700

    wes

--- SEMBAKO JAYA/admindashboard.cs
+++ SEMBAKO JAYA/admindashboard.cs
@@ -6,7 +6,8 @@ using System.Windows.Forms;
 namespace SEMBAKO_JAYA
 {
 	/// <summary>
-	/// Description of admindashboard.
+	/// panel admin yang berfungsi atau digunakan untuk kebutuhan admin seperti melihat barang,dan memanajemen pegawao (kasir)
+	///
 	/// </summary>
 	public partial class admindashboard : Form
 	{
@@ -21,12 +22,14 @@ namespace SEMBAKO_JAYA
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		///buttonview yang mengarah ke form CekSisabarang.cs
 		void button1_Click(object sender, EventArgs e)
 		{
 			ceksisabarang addmin = new ceksisabarang();
 			addmin.Show();
 			
 		}
+		///button yang terhubung ke form lihat pembeli yang digunakan untuk berpindah ke halaman Lihat_Pembeli.cs untuk memantau perkembangan pembeli
 		void button3_Click(object sender, EventArgs e)
 		{
 			Lihat_Pembeli buyer = new Lihat_Pembeli();
@@ -34,12 +37,15 @@ namespace SEMBAKO_JAYA
 			this.Hide();
 			
 		}
+		///button ini berguna untuk menuju ke CRUD admin jika ada orang melamar pekerjaan disini.
 		void button2_Click(object sender, EventArgs e)
 		{
 			ManajemenAdmin manadmin = new ManajemenAdmin();
 			manadmin.Show();
 			this.Hide();
 		}
+		///form log out yang sebenarnya hanya dihubungkan ke form Loginkasir dan di close ddengan code this.Close
+		//sehingga seakan terpasang fitur logout yang sesungguhnya
 		void label3_Click(object sender, EventArgs e)
 		{
 			this.Close();
